2b3800a471326509dd96f4ba8c0a30a8da29d315
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2023-10-03 23:08:00 +0900

    Portal로 postModal 추가

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -9,6 +9,8 @@ import BookmarkIcon from './ui/icons/BookmarkIcon';
 import { parseDate } from '@/util/date';
 import CommentForm from './CommentForm';
 import ActionBar from './ActionBar';
+import ModalPortal from './ui/ModalPortal';
+import PostModal from './PostModal';
 // import usePosts from '@/hooks/posts';
 // import ActionBar from './ActionBar';
 // import PostDetail from './PostDetail';
@@ -23,12 +25,19 @@ type Props = {
 
 export default function PostListCard({ post, priority = false }: Props) {
   const { userImage, username, image, comments, text, createdAt } = post;
+  const [openModal, setOpenModal] = useState(false)
   const handlePostComment = () => {}
   return (
     <article className='rounded-lg shadow-md border border-gray-200'>
       <Avatar image={userImage} />
       <span>{username}</span>
-      <Image src={image} alt={`photo by ${username}`} width={500} height={500} />
+      <Image 
+        src={image}
+        alt={`photo by ${username}`}
+        width={500}
+        height={500} 
+        onClick={() => setOpenModal(true)}
+      />
       <ActionBar post={post} onComment={handlePostComment}>
         <p>
           <span className='font-bold mr-1'>{username}</span>
@@ -37,10 +46,19 @@ export default function PostListCard({ post, priority = false }: Props) {
         {comments > 1 && (
           <button
             className='font-bold my-2 text-sky-700'
-            // onClick={() => setOpenModal(true)}
+            onClick={() => setOpenModal(true)}
           >{`View all ${comments} comments`}</button>
         )}
       </ActionBar>
+      {
+        openModal && <ModalPortal>
+          <PostModal onClose={() => setOpenModal(false)}>
+            {/* <div className='fixed top-0 left-0 w-full h-full bg-slate-500 z-50'> */}
+              포스트 상세페이지
+            {/* </div> */}
+          </PostModal>
+        </ModalPortal>
+      }
     </article>
   );
 }
